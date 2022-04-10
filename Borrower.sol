4101cb6e668b5c13193fc09926edefec3edca0bb
whn1527094475 <81701928+whn1527094475@users.noreply.github.com>
2022-04-10 17:07:07 +0800

    Update Borrower.sol

--- Borrower.sol
+++ Borrower.sol
@@ -1,20 +1,46 @@
+// SPDX-License-Identifier: SimPL-2.0
+pragma solidity ^0.8.7;
 contract Borrower {
-    uint256 public id;
+    string public id;
     string passwd;
     string name;
     uint256 reputation;
     bool public valid;
+    uint256 balance;
 
     constructor(
-        uint256 _id,
+        string memory _id,
         string memory _passwd,
         string memory _name,
         uint256 _reputation
-    ) {
+    ) public {
         id = _id;
         passwd = _passwd;
         name = _name;
         reputation = _reputation;
         valid = true;
     }
+
+    string InnerPasswd="123456";
+    function BorrowerLogin(string memory InputId) public returns(bool){
+      if(keccak256(bytes(InputId))==keccak256(bytes(InnerPasswd))) return true;
+      else return false;
+    }
+
+   function getBorrowerInfo() public view returns
+   (string memory,string memory,uint256,uint256){
+     return (id,name,reputation,balance);
+   }
+
+   function LoanApply(string memory Id,uint256 Amount) public {
+     // if(check一下reputation)??
+      balance += Amount;
+   }
+
+   function deleteBorrower() public returns (bool) {
+        valid = false;
+        if (valid == false) return true;
+        else return false;
+    }
+
 }
