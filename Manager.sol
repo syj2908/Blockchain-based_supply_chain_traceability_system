c7d047b5c36f2cd189356c57ab3ffa490d36e8f3
doYFf <103303314+doYFf@users.noreply.github.com>
2022-04-10 20:41:59 +0800

    Update Manager.sol

--- Manager.sol
+++ Manager.sol
@@ -1,9 +1,37 @@
-contract Manager {
-    uint256 public id;
-    string passwd;
+// SPDX-License-Identifier: SimPL-2.0
+pragma solidity ^0.8.7;
 
-    constructor(uint256 _id, string memory _passwd) {
+contract Manager {
+    string public id;
+    string passwd;
+    bool public valid;
+
+    constructor(string memory  _id, string memory _passwd) {
         id = _id;
         passwd = _passwd;
+        valid=true;
     }
+
+    function getManagerInfo()
+        public
+        view
+        returns (
+            string  memory
+        )
+    {
+        return (id);
+    }
+
+    function deleteManager() public returns (bool) {
+        valid = false;
+        if (valid == false) return true;
+        else return false;
+    }
+
+     string InnerPasswd="123456";
+
+    function ManagerLogin(string memory InputId) public returns(bool){
+      if(keccak256(bytes(InputId))==keccak256(bytes(InnerPasswd))) return true;
+      else return false;
+}
 }
