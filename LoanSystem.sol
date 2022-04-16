4f8d3760618979d2c735ff882ae9f8a555ed86d6
syj2908 <56108144+syj2908@users.noreply.github.com>
2022-04-15 19:13:42 -0700

    简化函数接口（地址相关）

--- LoanSystem.sol
+++ LoanSystem.sol
@@ -30,7 +30,6 @@ contract LoanSystem {
     CashFlow[] cashFlows;
     Transaction[] transactions;
 
-    uint256[] IDarr = [0, 0, 0]; //用户ID计数器
     uint256 cashFlowCount = 0; //资金流ID计数器
     uint256 TransactionCount = 0; //交易ID计数器
 
@@ -68,8 +67,9 @@ contract LoanSystem {
         return id;
     }
 
-    function deleteLender(address id) public returns (bool) {
+    function deleteLender() public returns (bool) {
         //删除指定放贷者
+        address id = msg.sender;
         for (uint256 i = 0; i < lenders.length; i++) {
             if (lenders[i].id() == id) {
                 bool result = lenders[i].deleteLender();
@@ -79,8 +79,9 @@ contract LoanSystem {
         }
     }
 
-    function deleteBorrower(address id) public returns (bool) {
+    function deleteBorrower() public returns (bool) {
         //删除指定借贷者
+        address id = msg.sender;
         for (uint256 i = 0; i < borrowers.length; i++) {
             if (borrowers[i].id() == id) {
                 bool result = borrowers[i].deleteBorrower();
@@ -90,8 +91,9 @@ contract LoanSystem {
         }
     }
 
-    function deleteManager(address id) public returns (bool) {
+    function deleteManager() public returns (bool) {
         //删除指定管理员
+        address id = msg.sender;
         for (uint256 i = 0; i < lenders.length; i++) {
             if (managers[i].id() == id) {
                 bool result = managers[i].deleteManager();
@@ -101,7 +103,7 @@ contract LoanSystem {
         }
     }
 
-    function getLenderInfo(address id)
+    function getLenderInfo()
         public
         view
         returns (
@@ -112,6 +114,7 @@ contract LoanSystem {
         )
     {
         //返回放贷者信息(除密码)
+        address id = msg.sender;
         for (uint256 i = 0; i < lenders.length; i++) {
             if (lenders[i].id() == id)
                 if (lenders[i].valid()) return lenders[i].getLenderInfo();
@@ -122,7 +125,7 @@ contract LoanSystem {
         return (addr, "NotFound", "NotFound", arr);
     }
 
-    function getBorrowerInfo(address id)
+    function getBorrowerInfo()
         public
         view
         returns (
@@ -135,6 +138,7 @@ contract LoanSystem {
         )
     {
         //返回借贷者信息(除密码)
+        address id = msg.sender;
         for (uint256 i = 0; i < borrowers.length; i++) {
             if (borrowers[i].id() == id)
                 if (borrowers[i].valid()) return borrowers[i].getBorrowerInfo();
@@ -157,30 +161,28 @@ contract LoanSystem {
     //     //功能未实现 待补充
     // }
 
-    function createCashFlow(
-        address lenderID,
-        address borrowerID,
-        uint256 cashNum
-    ) public returns (uint256) {
+    function createCashFlow(address borrowerID, uint256 cashNum)
+        public
+        returns (uint256)
+    {
         //发起一条资金流
         CashFlow cashflow = new CashFlow(cashFlowCount, cashNum);
         cashFlows.push(cashflow);
         for (uint256 i = 0; i < lenders.length; i++) {
-            if (lenders[i].id() == lenderID && lenders[i].valid())
+            if (lenders[i].id() == msg.sender && lenders[i].valid())
                 lenders[i].attachCashFlow(cashflow.id());
         }
         for (uint256 i = 0; i < borrowers.length; i++) {
             if (borrowers[i].id() == borrowerID && borrowers[i].valid())
                 borrowers[i].attachCashFlow(cashFlowCount);
         }
-        emit CreateCashFlow(lenderID, borrowerID, cashNum);
+        emit CreateCashFlow(msg.sender, borrowerID, cashNum);
         cashFlowCount++;
         return cashflow.id();
     }
 
     function createTransaction(
         uint256 cashFlowID,
-        address senderID,
         address receiverID,
         uint256 cashNum,
         string memory note
@@ -191,7 +193,7 @@ contract LoanSystem {
             Transaction transaction = new Transaction(
                 TransactionCount,
                 cashFlowID,
-                senderID,
+                msg.sender,
                 receiverID,
                 prevCash,
                 cashFlows[cashFlowID].currBalance(),
@@ -201,7 +203,7 @@ contract LoanSystem {
             transactions.push(transaction);
             TransactionCount++;
             cashFlows[cashFlowID].attachTransaction(transaction.id());
-            emit CreateTransaction(senderID, receiverID, cashFlowID, cashNum);
+            emit CreateTransaction(msg.sender, receiverID, cashFlowID, cashNum);
             return transaction.id();
         } else return type(uint256).max;
     }
@@ -236,58 +238,4 @@ contract LoanSystem {
         //查看交易的详细信息
         return transactions[transactionID].checkTransaction();
     }
-
-    //业务无关函数
-    function generateID(uint8 t) internal returns (string memory) {
-        //产生一个用户id 作为主键
-        IDarr[t]++;
-        return SysFunc.GenerateID(t, IDarr[t]);
-    }
-
-    /*
-    function strConcat(string memory _a, string memory _b)
-        internal
-        pure
-        returns (string memory)
-    {
-        //字符串拼接
-        bytes memory _ba = bytes(_a);
-        bytes memory _bb = bytes(_b);
-        string memory ret = new string(_ba.length + _bb.length);
-        bytes memory bret = bytes(ret);
-        uint256 k = 0;
-        for (uint256 i = 0; i < _ba.length; i++) bret[k++] = _ba[i];
-        for (uint256 i = 0; i < _bb.length; i++) bret[k++] = _bb[i];
-        return string(ret);
-    }
-    function generateID(uint8 t) internal returns (string memory) {
-        //产生一个用户id 作为主键
-        IDarr[t]++;
-        uint256 rawID = IDarr[t] - 1;
-        string memory strID;
-        if (rawID == 0) strID = "0";
-        else {
-            uint256 cp = rawID;
-            uint256 len = 0;
-            while (cp != 0) {
-                len++;
-                cp /= 10;
-            }
-            bytes memory bstr = new bytes(len);
-            while (rawID != 0) {
-                len--;
-                uint8 temp = (48 + uint8(rawID - (rawID / 10) * 10));
-                bytes1 b1 = bytes1(temp);
-                bstr[len] = b1;
-                rawID /= 10;
-            }
-            strID = string(bstr);
-        }
-        string memory prefix;
-        if (t == 0) prefix = "0";
-        else if (t == 1) prefix = "1";
-        else prefix = "2";
-        return strConcat(prefix, strID);
-    }
-    */
 }
--- SysFunc.sol
+++ SysFunc.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: SimPL-2.0
 pragma solidity ^0.8.7;
+
 library SysFunc {
     //业务无关的函数包
 
@@ -17,34 +18,5 @@ library SysFunc {
         for (uint256 i = 0; i < _ba.length; i++) bret[k++] = _ba[i];
         for (uint256 i = 0; i < _bb.length; i++) bret[k++] = _bb[i];
         return string(ret);
-    }
-
-    function GenerateID(uint8 t,uint256 IDarr) public returns (string memory) {
-        //产生一个用户id 作为主键
-        uint256 rawID = IDarr - 1;
-        string memory strID;
-        if (rawID == 0) strID = "0";
-        else {
-            uint256 cp = rawID;
-            uint256 len = 0;
-            while (cp != 0) {
-                len++;
-                cp /= 10;
-            }
-            bytes memory bstr = new bytes(len);
-            while (rawID != 0) {
-                len--;
-                uint8 temp = (48 + uint8(rawID - (rawID / 10) * 10));
-                bytes1 b1 = bytes1(temp);
-                bstr[len] = b1;
-                rawID /= 10;
-            }
-            strID = string(bstr);
-        }
-        string memory prefix;
-        if (t == 0) prefix = "0";
-        else if (t == 1) prefix = "1";
-        else prefix = "2";
-        return strConcat(prefix, strID);
     }
 }
